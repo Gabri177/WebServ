2ea4ec1e57b50123e306ec12f3fcefae80f05dc5
Gabri177 <forestcat177@gmail.com>
2024-07-17 21:19:35 +0200

    modified main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include "../include/ConfigInfo.hpp"
 #include "../include/Server.hpp"
 
-int	main(){
+int	main(int argc, char **argv){
 
-	ConfigInfo	_config("./config/default.ini");
+	if (argc == 1)
+		ConfigInfo	_config("./config/default.ini");
+	else if (argc == 2)
+		ConfigInfo 	_config(arv[1]);
+	else{
+
+		std::cout << "Error: Wrong arguments!!!" << std::endl;
+	}
 	Server		_serv(_config);
 
-	std::cout << _config["server"]["port"] << std::endl;
+	//std::cout << _config["server"]["port"] << std::endl;
 	_serv.start();
 
 	return 0;
