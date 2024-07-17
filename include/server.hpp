632fd8979d36ad4ebc13b0a7d8e231dba4758b32
GGabriel003 <108942046+GGabriel003@users.noreply.github.com>
2024-07-17 02:40:13 +0200

    add header needed in Server.hpp

--- include/server.hpp
+++ include/server.hpp
@@ -1,12 +1,17 @@
 #ifndef	SERVER_HPP
 # define SERVER_HPP
+# include <iostream>
+# include <algorithm>
+# include <sys/socket.h>
+# include <unistd.h>
+# include <netinet/in.h>
+# include <string>
+# include <cstdlib>
+# include <cstdio>
 
 class Server {
 
-	private:
 
-	public:
-	
 };
 
 #endif
