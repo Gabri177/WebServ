41a84ffb93e617414a96454e73057b32764d2be9
Gabri177 <forestcat177@gmail.com>
2024-07-22 02:36:11 +0200

    ParseURL.hpp for read key y val in GET request

--- parse_url_test.cpp
+++ parse_url_test.cpp
@@ -1,4 +1,4 @@
-#include "ParserURL.hpp"
+#include "./include/ParserURL.hpp"
 
 
 int main(void){
@@ -6,12 +6,14 @@ int main(void){
 
 	std::map<std::string, std::string>  key_val;
 
-	key_val = ParserURL<std::string, std::string>::get_url_key_val("www.baidu.com/index.html?aaaa=1&bbb=222222222222222222");
+	key_val = ParserURL<std::string, std::string>::get_url_key_val("www.baidu.com/index.html");
 
-	for (std::map<std::string, std::string>::iterator it = key_val.begin(); it != key_val.end(); it ++){
-
-		std::cout << "key: " << it->first << "\t" << "Val: " << it->second << std::endl;
-	}
+	if (!key_val.empty())
+		for (std::map<std::string, std::string>::iterator it = key_val.begin(); it != key_val.end(); it ++)
+			std::cout << "key: " << it->first << "\t" << "Val: " << it->second << std::endl;
+	else
+		std::cout << "Nooooooooooooooooooooooooooooooo" << std::endl;
+	
 
 
 
